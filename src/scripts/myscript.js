134e1e6bcf7a7ac75aa8488eb325e8c0ea7e1025
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-03-20 17:03:30 +0300

    main page

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -1,5 +1,14 @@
 //Переключение табов
 
 $(document).ready(function () {  
+    $('.scrollbar-dynamic').scrollbar();
     
+      setTimeout(function(){
+        $('.road-news-content').each(function (index, element) {
+            var el = $('.road-news-content').eq(index);
+            el.find('.road-news-right').css('max-height', el.find('.road-news-left').innerHeight());
+            console.log(el.find('.road-news-left').find('.road-news-left__title').html());
+            console.log(el.find('.road-news-left').height());            
+          });
+            }, 1000);  
 });
