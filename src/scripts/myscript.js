558d623fe3618ffcb838958e13ba365b38bba1e8
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-03-21 15:49:48 +0300

    inner pages initial

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -1,7 +1,11 @@
 //Переключение табов
 
 $(document).ready(function () {  
-    $('.scrollbar-dynamic').scrollbar();
+    $('.scrollbar-dynamic').scrollbar();    
+    $( ".search-page-option select" ).click(function(event) {
+        event.preventDefault();      
+      });      
+    $('.search-page-option select').SumoSelect();
     
       setTimeout(function(){
         $('.road-news-content').each(function (index, element) {
@@ -10,5 +14,18 @@ $(document).ready(function () {
             console.log(el.find('.road-news-left').find('.road-news-left__title').html());
             console.log(el.find('.road-news-left').height());            
           });
-            }, 1000);  
+            }, 1000);
+            
+    //   $(document).on('click', '.header-menu a:not(active)', function(){
+    //     var page_number = $(this).index();                
+    //     $(this).addClass('active').siblings('.header-menu a').removeClass('active');        
+    // });
+          $('.header-menu a').each(function () {
+            var location = window.location.href;
+            var link = this.href;
+            if (location == link) {
+                $(this).addClass('active');
+            }
+          });        
+    
 });
