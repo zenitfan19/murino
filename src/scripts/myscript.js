4c743867074320433f7430ab276543fcae256e8d
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-03-22 14:59:16 +0300

    preview

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -14,18 +14,67 @@ $(document).ready(function () {
             console.log(el.find('.road-news-left').find('.road-news-left__title').html());
             console.log(el.find('.road-news-left').height());            
           });
-            }, 1000);
-            
-    //   $(document).on('click', '.header-menu a:not(active)', function(){
-    //     var page_number = $(this).index();                
-    //     $(this).addClass('active').siblings('.header-menu a').removeClass('active');        
-    // });
+            }, 1000);            
+    
           $('.header-menu a').each(function () {
             var location = window.location.href;
             var link = this.href;
             if (location == link) {
                 $(this).addClass('active');
             }
-          });        
+          });   
+          
+          
+          $(document).on('click', '.hamburger', function () {
+            $(document).find('.header-mobile-menu').addClass('active fadeInLeft animated');
+            if ($(this).hasClass('is-active')) {
+                  $(document).find('.header-mobile-menu').removeClass('fadeInLeft');
+                  $(document).find('.header-mobile-menu').addClass('fadeOutLeft');
+            } else {
+                  $(document).find('.header-mobile-menu').removeClass('fadeOutLeft');
+            }
+            $(this).toggleClass('is-active');
+      });
+      // $(window).scroll(function () {
+      //       if ($(this).scrollTop() > 1) {
+      //             $(document).find('.header-mobile-menu').removeClass('fadeInLeft');
+      //             $(document).find('.header-mobile-menu').addClass('fadeOutLeft');
+      //             $(document).find('.hamburger').removeClass('is-active');
+      //       }
+      // });
+      $(document).mouseup(function (e) { // событие клика по веб-документу
+            var div = $('.header-mobile-menu'); // тут указываем сласс элемента
+            if ($('.hamburger').is(e.target) || $('.hamburger-box').is(e.target) || $('.hamburger-inner').is(e.target)) {
+                  return;
+            }
+            if (!div.is(e.target) && div.has(e.target).length === 0) { // если клик был не по нашему блоку и не по его дочерним элементам
+                  div.removeClass('fadeInLeft');
+                  div.addClass('fadeOutLeft'); // скрываем его
+                  $(document).find('.hamburger').removeClass('is-active'); // меняем значок гамбургера
+            }
+      });
+      $(function () {
+            $(window).resize(function () {
+                  if (screen.width > 1024) {
+                        $(document).find('.hamburger').removeClass('is-active');
+                        $(document).find('.header-mobile-menu').removeClass('active');
+                        $(document).find('.header-mobile-menu').removeClass('fadeOutLeft');
+                        $(document).find('.header-mobile-menu').removeClass('animated');
+                  }
+                  if (screen.width < 1249) {                    
+                    $(document).find('.top-news-content > .main-news-left-block__new').each(function (index, element) {                      
+                      var el = $('.top-news-content > .main-news-left-block__new').eq(index);
+                      if(index > 2) {
+                        el.css('display', 'none');                          
+                      }          
+                    });                    
+              } else {
+                $(document).find('.top-news-content > .main-news-left-block__new').each(function (index, element) {
+                  var el = $('.top-news-content > .main-news-left-block__new').eq(index);
+                  el.css('display', 'block');
+                }); 
+              }
+            });
+      });
     
 });
